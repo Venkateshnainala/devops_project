76745917bbb6d6a18b8c941cbc6fdbdc41f44cbf
Venkateshnainala <venkateshnainala@gmail.com>
2026-09-17 17:58:53 -0700

    modefing sampleapplication

--- src/main/java/com/tech/sample/SampleApplication.java
+++ src/main/java/com/tech/sample/SampleApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
-
+	
+	@GetMapping("/")
+	public String home() {
+	       return "DevOps Pipeline Live on AWS!";
+	}
 }
